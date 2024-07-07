faba43518e63fb7e2973200b4ad0fe8dbfcedaf0
ZiyadZK <kakangtea74@gmail.com>
2024-07-07 22:35:59 +0700

    fixing data ijazah

--- database/function/F_Ijazah.js
+++ database/function/F_Ijazah.js
@@ -1,16 +1,39 @@
 const { Op } = require("sequelize")
 const { getSocketIO } = require("../../socket")
 const { M_DataIjazah } = require("../model/M_Ijazah")
+const { M_DataAlumni } = require("../model/M_Alumni")
 
 exports.F_DataIjazah_getAll = async () => {
     try {
         const data = await M_DataIjazah.findAll({
-            raw: true
+            raw: true,
+            include: [
+                {
+                    model: M_DataAlumni,
+                    as: 'data_alumni'
+                }
+            ]
         })
+
+        const formattedData = data.map(value => ({
+            no: value['no'],
+            kelas: value['data_alumni.kelas'],
+            jurusan: value['data_alumni.jurusan'],
+            rombel: value['data_alumni.rombel'],
+            nama_siswa: value['data_alumni.nama_siswa'],
+            nis: value['data_alumni.nis'],
+            nisn: value['data_alumni.nisn'],
+            tahun_masuk: value['data_alumni.tahun_masuk'],
+            tanggal_keluar: value['data_alumni.tanggal_keluar'],
+            tanggal_diambil: value['tanggal_diambil'],
+            no_ijazah: value['no_ijazah'],
+            nama_pengambil: value['nama_pengambil'],
+            status: value['status'],
+        }))
 
         return {
             success: true,
-            data: data
+            data: formattedData
         }
     } catch (error) {
         console.log(error.message)
